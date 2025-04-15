92aa82df79aec7e7b07ded7f25c4e136ac8247ef
Rutuja choudhar <rutuja_choudhar_comp@moderncoe.edu.in>
2025-04-15 22:24:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
+from fastapi import WebSocket, WebSocketDisconnect
 
 app = FastAPI()
 
@@ -12,7 +13,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+from fastapi import WebSocket, WebSocketDisconnect
 # Connected clients
 clients = []
 
@@ -23,8 +24,11 @@ async def websocket_endpoint(websocket: WebSocket):
     try:
         while True:
             data = await websocket.receive_text()
+            # Broadcast to all clients
             for client in clients:
-                if client != websocket:
-                    await client.send_text(data)
+                await client.send_text(data)
     except WebSocketDisconnect:
         clients.remove(websocket)
+
+# Connected clients
+
